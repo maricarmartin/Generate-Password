61a080e9675f2e3575e1c626133b22bee4fb0f4e
Maricar Martin <115593469+maricarmartin@users.noreply.github.com>
2022-11-10 23:03:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Assignment code here
+
+var length = Number(prompt("Enter a password length between 8 and 128")),
+  charType = prompt("Enter a character type: special, numeric, uppercase, lowercase."),
+  password = generatePassword("dsk@SiDa");
+document.getElementById("display").value = password;
+document.getElementById('copy-btn').addEventListener('click', copyPassword);
+
+var chars = "0123456789abcdefghijklmonpqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
+var password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+ }
+
+function generatePassword() {
+  var charSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  };
+  var charSet = charSets[charType.toLowerCase()] || charSets.lowercase;
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+
+function copyPassword() {
+ document.getElementById("password").value = password;
+  alert("Password copied to clipboard!");
+}
